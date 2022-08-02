c6551c24c391827cd3207bb1ec8978b145a31cf0
BurhanuddinRaja <mails4burhan@gmail.com>
2022-08-02 14:46:32 +0530

    updated

--- pages/signup.js
+++ pages/signup.js
@@ -16,7 +16,7 @@ const Signup = () => {
                                 src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg" className="w-full" alt="Phone image" />
                         </div>
                         <div className="md:w-8/12 lg:w-5/12 lg:ml-20">
-                            <form onSubmit={handleSignUp}>
+                            <form>
 
                                 <div className="mb-6">
                                     <input type="text" className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Username" name="username" id="username" onChange={onChange} value={credentials.username} />
