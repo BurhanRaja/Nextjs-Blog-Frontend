948dfe44688eab09356b00462cb2c8f520fc635b
BurhanuddinRaja <mails4burhan@gmail.com>
2022-08-02 14:41:55 +0530

    updated

--- pages/signup.js
+++ pages/signup.js
@@ -32,9 +32,7 @@ const Signup = () => {
                                 </div>
 
                                 <div className="flex justify-between items-center mb-6">
-                                    <a
-                                        href="#!"
-                                        className="text-blue-600 hover:text-blue-700 focus:text-blue-700 active:text-blue-800 duration-200 transition ease-in-out">Forgot password?</a>
+                                    <a href="#!" className="text-blue-600 hover:text-blue-700 focus:text-blue-700 active:text-blue-800 duration-200 transition ease-in-out">Forgot password?</a>
                                 </div>
 
 
